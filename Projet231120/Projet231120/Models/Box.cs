df680c61e707e0fae99cff061692dcb1e25f0740
FlorentT-Dev <59725973+FlorentT-Dev@users.noreply.github.com>
2020-11-23 10:46:36 +0100

    classe Box

--- Projet231120/Projet231120/Models/Box.cs
+++ Projet231120/Projet231120/Models/Box.cs
@@ -8,5 +8,39 @@ namespace Projet231120.Models
 {
     class Box
     {
+        #region Attributs
+        public static List<Box> collClasseBox = new List<Box>();
+        private int _id;
+        private string _adresse;
+        private int _gpsX;
+        private int _gpsY;
+        private List<Compartiment> _lesCompartiments;
+        private List<Livreur> _lesLivreur;
+        #endregion
+
+        #region Constructeurs
+        public Box(int id, string adresse, int gpsX, int gpsY)
+        {
+            _id = id;
+            _adresse = adresse;
+            _gpsX = gpsX;
+            _gpsY = gpsY;
+            _lesLivreur = new List<Livreur>();
+            _lesCompartiments = new List<Compartiment>();
+            collClasseBox.Add(this);
+        }
+        #endregion
+
+        #region Getters Setters
+        public int Id { get => _id; set => _id = value; }
+        public string Adresse { get => _adresse; set => _adresse = value; }
+        public int GpsX { get => _gpsX; set => _gpsX = value; }
+        public int GpsY { get => _gpsY; set => _gpsY = value; }
+        public List<Compartiment> LesCompartiments { get => _lesCompartiments; set => _lesCompartiments = value; }
+        public List<Livreur> LesLivreur { get => _lesLivreur; set => _lesLivreur = value; }
+        #endregion
+
+        #region Methodes
+        #endregion
     }
 }
