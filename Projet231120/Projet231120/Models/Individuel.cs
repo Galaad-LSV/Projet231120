ee9caa0d3546572246d5f59f8fd18d023211b65f
FlorentT-Dev <59725973+FlorentT-Dev@users.noreply.github.com>
2020-11-23 11:06:28 +0100

    Individuel

---
+++ Projet231120/Projet231120/Models/Individuel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projet231120.Models
+{
+    class Individuel : Client
+    {
+        #region Attributs
+        private int _coupon;
+        #endregion
+
+        #region Constructeurs
+        public Individuel(int coupon)
+        {
+            _coupon = coupon;
+        }
+        #endregion
+
+        #region Getters Setters
+        public int Coupon { get => _coupon; set => _coupon = value; }
+        #endregion
+
+        #region Methodes
+        #endregion
+    }
+}
