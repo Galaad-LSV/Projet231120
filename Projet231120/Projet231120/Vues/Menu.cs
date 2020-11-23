2f4b7372ec2cae372fbdce3c7cd5184ee2f6e766
FlorentT-Dev <59725973+FlorentT-Dev@users.noreply.github.com>
2020-11-23 15:06:39 +0100

    Menu fini

--- Projet231120/Projet231120/Vues/Menu.cs
+++ Projet231120/Projet231120/Vues/Menu.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Projet231120.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,46 @@ namespace Projet231120.Vues
         public Menu()
         {
             InitializeComponent();
+        }
+
+        private void villeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void boxToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Box from1 = new Box();
+            from1.Show();
+            this.Close();
+        }
+
+        private void compartimentToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Compartiment from1 = new Compartiment();
+            from1.Show();
+            this.Close();
+        }
+
+        private void colisToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Colis from1 = new Colis();
+            from1.Show();
+            this.Close();
+        }
+
+        private void livreurToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Livreur from1 = new Livreur();
+            from1.Show();
+            this.Close();
+        }
+
+        private void clientToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Client from1 = new Client();
+            from1.Show();
+            this.Close();
         }
     }
 }
