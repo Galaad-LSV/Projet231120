82f42d718936e9b73490f9484d060eb9cb0a28f7
FlorentT-Dev <59725973+FlorentT-Dev@users.noreply.github.com>
2020-11-23 11:46:31 +0100

    Colis

---
+++ Projet231120/Projet231120/Models/Colis.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projet231120.Models
+{
+    class Colis
+    {
+        #region Attributs
+        private static List<Colis> collClasseColis = new List<Colis>();
+        private int _id;
+        private string _volume;
+        #endregion
+
+        #region Constructeur
+        public Colis(int id, string volume)
+        {
+            _id = id;
+            _volume = volume;
+            collClasseColis.Add(this);
+        }
+        #endregion
+
+        #region Getters-Setters
+        internal static List<Colis> CollClasseColis { get => collClasseColis; set => collClasseColis = value; }
+        public int Id { get => _id; set => _id = value; }
+        public string Volume { get => _volume; set => _volume = value; }
+        #endregion
+
+        #region Méthodes
+        #endregion
+
+    }
+}
