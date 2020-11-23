99bdd011cc4ab409448a40b51d15982f349b3651
thierrysio <thierry28220@icloud.com>
2020-11-23 11:23:14 +0100

    classe client

---
+++ Projet231120/Projet231120/Models/Client.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projet231120.Models
+{
+    class Client
+    {
+        #region Attributs
+        #endregion 
+        #region Constructeurs
+        #endregion 
+        #region Getters Setters
+        #endregion 
+        #region Methodes
+        #endregion 
+    }
+}
