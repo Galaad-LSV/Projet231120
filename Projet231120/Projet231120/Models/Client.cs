d56d8fe5c52045e81313cfac1cbd5bd7e3e8c8de
thierrysio <thierry28220@icloud.com>
2020-11-23 11:40:08 +0100

    client

--- Projet231120/Projet231120/Models/Client.cs
+++ Projet231120/Projet231120/Models/Client.cs
@@ -6,15 +6,42 @@ using System.Threading.Tasks;
 
 namespace Projet231120.Models
 {
-    class Client
+    public abstract class Client
     {
         #region Attributs
-        #endregion 
+        public static List<Client> CollClasseClient = new List<Client>();
+        private int _id;
+        private string _nom;
+        private string _prenom;
+        private string _adresse;
+        private Ville _laVille;
+        private List<Colis> _lesColis;
+
+
+        #endregion
         #region Constructeurs
-        #endregion 
+        protected Client(int id, string nom, string prenom, string adresse, Ville laVille)
+        {
+            _id = id;
+            _nom = nom;
+            _prenom = prenom;
+            _adresse = adresse;
+            _laVille = laVille;
+            _lesColis = new List<Colis>();
+            Client.CollClasseClient.Add(this);
+        }
+        #endregion
         #region Getters Setters
-        #endregion 
+        public int Id { get => _id; set => _id = value; }
+        public string Nom { get => _nom; set => _nom = value; }
+        public string Prenom { get => _prenom; set => _prenom = value; }
+        public string Adresse { get => _adresse; set => _adresse = value; }
+        public Ville LaVille { get => _laVille; set => _laVille = value; }
+        public List<Colis> LesColis { get => _lesColis; set => _lesColis = value; }
+
+
+        #endregion
         #region Methodes
-        #endregion 
+        #endregion
     }
 }
